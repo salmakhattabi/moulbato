df1f880262fa256fdacb588acb09cdcbd53c1ef5
loubna <88745101+loucharr@users.noreply.github.com>
2021-11-02 11:47:03 +0100

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
+  #get 'dashboard', to: 'pages#dashboard '
+
+  resources :boats do
+    resouces :orders, only: [:index, :new, :create]
+  end
+  resouces :orders, only: [:edit, :show, :destroy, :update]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
